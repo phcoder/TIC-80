4c6a9f45a71b49912cb70b5e1a48fd086ec412f8
Nesbox <grigoruk@gmail.com>
2020-03-15 19:12:24 +0300

    arpeggio switch

--- src/sfx.c
+++ src/sfx.c
@@ -206,6 +206,35 @@ static void drawVolumeStereo(Sfx* sfx, s32 x, s32 y)
 	}
 }
 
+static void drawArppeggioSwitch(Sfx* sfx, s32 x, s32 y)
+{
+	tic_mem* tic = sfx->tic;
+
+	static const char Label[] = "DOWN";
+
+	enum {Width = (sizeof Label - 1) * TIC_ALTFONT_WIDTH - 1, Height = TIC_FONT_HEIGHT};
+
+	tic_sample* effect = getEffect(sfx);
+
+	{
+		tic_rect rect = {x, y, Width, Height};
+
+		bool hover = false;
+		if(checkMousePos(&rect))
+		{
+			setCursor(tic_cursor_hand);
+			hover = true;
+
+			showTooltip("up/down arpeggio");
+
+			if(checkMouseClick(&rect, tic_mouse_left))
+				effect->reverse = ~effect->reverse;
+		}
+
+		tic->api.text(tic, Label, rect.x, rect.y, effect->reverse ? tic_color_5 : hover ? tic_color_14 : tic_color_15, true);
+	}
+}
+
 static void drawCanvas(Sfx* sfx, s32 x, s32 y, s32 canvasTab)
 {
 	tic_mem* tic = sfx->tic;
@@ -222,7 +251,7 @@ static void drawCanvas(Sfx* sfx, s32 x, s32 y, s32 canvasTab)
 	switch(canvasTab)
 	{
 	case SFX_VOLUME_PANEL:	drawVolumeStereo(sfx, x + 2, y + 9); break;
-	case SFX_CHORD_PANEL:	break;
+	case SFX_CHORD_PANEL:	drawArppeggioSwitch(sfx, x + 2, y + 9); break;
 	case SFX_PITCH_PANEL:	break;
 	}
 
